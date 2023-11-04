508e124672a0c52abf9677dcdc528e1bb578fae1
Marc Pellicer <marcpelli3@gmail.com>
2023-11-04 18:12:45 +0100

    Project created

---
+++ src/main/java/Ej02/UD22_2/App.java
@@ -0,0 +1,13 @@
+package Ej02.UD22_2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/Ej02/UD22_2/AppTest.java
@@ -0,0 +1,20 @@
+package Ej02.UD22_2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
